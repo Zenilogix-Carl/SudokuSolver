77be20b0de1253b1df3cae9f29623487a1c6879d
Carl Robinson <Carl.Robinson@zenilogix.com>
2015-09-24 23:46:37 -0400

    Initial commit

---
+++ Solver/Cell.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solver
+{
+    public class Cell : INotifyPropertyChanged
+    {
+
+        private int _canBeInRow;
+        private int _canBeInColumn;
+        private int _canBeInSector;
+        private int _row;
+        private int _column;
+        private int _sector;
+        private int _value;
+
+        public int CanBeInRow
+        {
+            get { return _canBeInRow; }
+            set { _canBeInRow = value; NotifyPropertyChanged();}
+        }
+
+        public int CanBeInColumn
+        {
+            get { return _canBeInColumn; }
+            set { _canBeInColumn = value; NotifyPropertyChanged(); }
+        }
+
+        public int CanBeInSector
+        {
+            get { return _canBeInSector; }
+            set { _canBeInSector = value; NotifyPropertyChanged(); }
+        }
+
+        public int Row
+        {
+            get { return _row; }
+            set { _row = value; NotifyPropertyChanged(); }
+        }
+
+        public int Column
+        {
+            get { return _column; }
+            set { _column = value; NotifyPropertyChanged(); }
+        }
+
+        public int Sector
+        {
+            get { return _sector; }
+            set
+            { 
+                _sector = value;
+                NotifyPropertyChanged(); 
+            }
+        }
+
+        public int Value
+        {
+            get { return _value; }
+            set
+            {
+                _value = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void NotifyPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
---
+++ Solver/Puzzle.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solver
+{
+    public class Puzzle
+    {
+        private const int Any = 0x1f;
+        public Cell[] Cells { get; set; }
+
+        public Puzzle()
+        {
+            Cells = new Cell[27];
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    var index = (i*9) + j;
+                    var sector = ((i/3)*3) + (j/3);
+                    Cells[index] = new Cell
+                    {
+                        Row = i,
+                        Column = j,
+                        Sector = sector,
+                        Value = 0,
+                        CanBeInRow = Any,
+                        CanBeInColumn = Any,
+                        CanBeInSector = Any
+                    };
+
+                    Cells[index].PropertyChanged += OnPropertyChanged;
+                }
+            }
+        }
+
+        private void OnPropertyChanged(object sender, PropertyChangedEventArgs propertyChangedEventArgs)
+        {
+            var cell = sender as Cell;
+            switch (propertyChangedEventArgs.PropertyName)
+            {
+                case "Value":
+                    break;
+                case "CanBeInRow":
+                    break;
+                case "CanBeInColumn":
+                    break;
+                case "CanBeInSector":
+                    break;
+            }
+        }
+    }
+}
